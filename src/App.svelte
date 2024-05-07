196a5706d7d88cb2995a32de01d71bc757ff5266
Justas <zhongli3137@gmail.com>
2024-05-07 18:43:40 +0300

    Removed .prettierrc and added login prototype page

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,46 @@
 <script>
-    let name = 'Justas';
+  let username = "";
+  let password = "";
+
+  function login(event) {
+    event.preventDefault();
+  }
+
+  function register() {}
 </script>
 
-<p>Hello {name}!</p>
-<p>Testo!</p>
-<p>Working?</p>
-{name}
+<h1>Larben</h1>
+<p>Sveiki atvykę į Lietuvos Archeologijos Bendruomenę!</p>
+<p>Įveskite vartojo vardą ir slaptažodį, kad prisijungtumėte.</p>
+
+<form>
+  <label for="username">Vartotojo vardas</label>
+  <input bind:value={username} type="text" id="username" name="username" />
+
+  <label for="password" id="password-lbl">Slaptažodis</label>
+  <input bind:value={password} type="password" id="password" name="password" />
+
+  <button on:click={login} id="login-btn">Prisijungti</button>
+</form>
+
+<hr />
+<div class="bot-region">
+  <p>Neturi paskyros? Spausk registracijos mygtuką.</p>
+  <button on:click={register} id="reg-btn">Registruotis</button>
+</div>
 
 <style>
-    p {
-        color: red;
-    }
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  #password-lbl {
+    margin-top: 7px;
+  }
+
+  #login-btn {
+    display: block;
+    margin: 1rem 0;
+  }
 </style>
